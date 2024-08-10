af3ac72a55ae9b8e448f4b48f48d8a59075c1cb8
Najil Parakkal <najilparakkal@gmail.com>
2024-08-10 21:43:37 +0530

    hosting

--- FRONTEND/src/API/services/admin/adminAuthServices.ts
+++ FRONTEND/src/API/services/admin/adminAuthServices.ts
@@ -1,20 +1,17 @@
 import { authAxiosInstance } from "./axios/AxiosInstance";
 import Cookies from "js-cookie";
 
-
-
-
 export const adminLogin = async (values: any): Promise<boolean> => {
-    try {
-        const response = await authAxiosInstance.post("admin/login", values);
-        if (response.status === 200) {
-            Cookies.set("adminToken",response.data.response.accessToken)
-            return true
-        } else {
-            return false
-        }
-    } catch (error) {
-        console.log(error);
-        return false; 
+  try {
+    const response = await authAxiosInstance.post("admin/login", values);
+    if (response.status === 200) {
+      Cookies.set("adminToken", response.data.response.accessToken);
+      return true;
+    } else {
+      return false;
     }
-}
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+};
